9226cf24de686b3c7e67c03a00b10e126875ff2a
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-04-23 01:09:27 -0600

    1

---
+++ EnumPractice_2/EnumPractice_2/MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice_2
+{
+    class MainClass
+    {
+
+        enum Day
+        {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri,
+            Sat
+        }
+
+        static void Main(string[] args)
+        {
+            int var1 = (int) Day.Sun;
+            int var2 = (int) Day.Fri;
+
+            Console.WriteLine("Sun = {0}", var1);
+            Console.WriteLine("Fri = {0}", var2);
+            Console.ReadKey();
+        }
+    }
+}
